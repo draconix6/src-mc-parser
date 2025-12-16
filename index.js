845e60cca620ce39d484a1e0914635ef737c8ebd
draconix6 <gddraconic@gmail.com>
2025-12-16 17:44:49 +1300

    get rid of credential file crap

--- index.js
+++ index.js
@@ -1,56 +1,39 @@
 // import path from 'node:path';
 import process from 'node:process';
-// import {authenticate} from '@google-cloud/local-auth';
 import {GoogleAuth} from 'google-auth-library';
 import {google} from 'googleapis';
-// import {readFile} from 'fs';
-
-// The scope for reading spreadsheets.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 
 /**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * Gets cell values from a spreadsheet.
+ * @param {string} spreadsheetId The ID of the spreadsheet.
+ * @param {string} range The range of cells to retrieve.
+ * @return {Promise<object>} The response from the get request.
  */
-async function listMajors() {
+async function getValues(spreadsheetId, range) {
   // Authenticate with Google and get an authorized client.
   const auth = new GoogleAuth({
-    scopes: SCOPES,
+    scopes: 'https://www.googleapis.com/auth/spreadsheets',
   });
 
-    const keys = JSON.parse(data)
-    const authClient = JWT.fromJSON(keys);
-
-    // Create a new Sheets API client.
-    const sheets = google.sheets({version: 'v4', auth});
-    // Get the values from the spreadsheet.
-    const result = await sheets.spreadsheets.values.get({
-      spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-      range: 'Class Data!A2:E',
-    });
-    const rows = result.data.values;
-    if (!rows || rows.length === 0) {
-      console.log('No data found.');
-      return;
-    }
-    console.log('Name, Major:');
-    // Print the name and major of each student.
-    rows.forEach((row) => {
-      // Print columns A and E, which correspond to indices 0 and 4.
-      console.log(`${row[0]}, ${row[4]}`);
-    });
-  // });
+  // Create a new Sheets API client.
+  const service = google.sheets({version: 'v4', auth});
+  // Get the values from the specified range.
+  const result = await service.spreadsheets.values.get({
+    spreadsheetId,
+    range,
+  });
+  const numRows = result.data.values ? result.data.values.length : 0;
+  console.log(`${numRows} rows retrieved.`);
+  return result;
 }
-
-await listMajors();
 
 import express from 'express';
 const app = express()
 
 app.use(express.static('public'));
 app.get('/', async (req, res) => {
-  appendToSheet();
-  res.send('Hello World')}
+  // appendToSheet();
+  res.send(await getValues("1qsPHFXee0SB0Pc8KYxLWa-u18xjFoAymzu4ALyI17gs", "Sheet1"))}
 );
 
 const PORT = process.env.PORT || 8080;
