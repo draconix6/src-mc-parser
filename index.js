3d9d42a39ef74e643b711453db9e19bb49c70cfa
draconix6 <gddraconic@gmail.com>
2025-12-16 19:12:03 +1300

    use write request, restructure for cloud run job

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// import path from 'node:path';
-import process from 'node:process';
 import {GoogleAuth} from 'google-auth-library';
 import {google} from 'googleapis';
 
 /**
- * Gets cell values from a spreadsheet.
- * @param {string} spreadsheetId The ID of the spreadsheet.
- * @param {string} range The range of cells to retrieve.
- * @return {Promise<object>} The response from the get request.
+ * Updates values in a spreadsheet.
+ * @param {string} spreadsheetId The ID of the spreadsheet to update.
+ * @param {string} range The range of cells to update.
+ * @param {string} valueInputOption How the input data should be interpreted.
+ * @param {(string[])[]} _values A 2D array of values to update.
+ * @return {Promise<object>} The response from the update request.
  */
-async function getValues(spreadsheetId, range) {
+async function updateValues(spreadsheetId, range, valueInputOption, _values) {
   // Authenticate with Google and get an authorized client.
   const auth = new GoogleAuth({
     scopes: 'https://www.googleapis.com/auth/spreadsheets',
@@ -17,28 +17,47 @@ async function getValues(spreadsheetId, range) {
 
   // Create a new Sheets API client.
   const service = google.sheets({version: 'v4', auth});
-  // Get the values from the specified range.
-  const result = await service.spreadsheets.values.get({
+
+  // The values to update in the spreadsheet.
+  let values = [
+    [
+      "hi"
+    ],
+    // Additional rows ...
+  ];
+
+  // Create the request body.
+  const resource = {
+    values,
+  };
+
+  // Update the values in the spreadsheet.
+  const result = await service.spreadsheets.values.update({
     spreadsheetId,
     range,
+    valueInputOption,
+    resource,
   });
-  const numRows = result.data.values ? result.data.values.length : 0;
-  console.log(`${numRows} rows retrieved.`);
+
+  // Log the number of updated cells.
+  console.log('%d cells updated.', result.data.updatedCells);
   return result;
 }
 
-import express from 'express';
-const app = express()
+updateValues("1qsPHFXee0SB0Pc8KYxLWa-u18xjFoAymzu4ALyI17gs", "Sheet1!B2", "USER_ENTERED");
 
-app.use(express.static('public'));
-app.get('/', async (req, res) => {
-  // appendToSheet();
-  res.send(await getValues("1qsPHFXee0SB0Pc8KYxLWa-u18xjFoAymzu4ALyI17gs", "Sheet1"))}
-);
+// import express from 'express';
+// const app = express()
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(
-    `Hello from Cloud Run! The container started successfully and is listening for HTTP requests on ${PORT}`
-  );
-});
+// app.use(express.static('public'));
+// app.get('/', async (req, res) => {
+//   // appendToSheet();
+//   res.send(await getValues("1qsPHFXee0SB0Pc8KYxLWa-u18xjFoAymzu4ALyI17gs", "Sheet1"))}
+// );
+
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//   console.log(
+//     `Hello from Cloud Run! The container started successfully and is listening for HTTP requests on ${PORT}`
+//   );
+// });
